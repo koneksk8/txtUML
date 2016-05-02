a87662ca623883a6e84dfca2a35812dfa5f596cc
András Dobreff <dobreffandras@gmail.com>
2016-05-02 22:09:38 +0200

    Adding connetors to the diagram

--- dev/plugins/hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/api/CompositeDiagramElementsController.java
+++ dev/plugins/hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/api/CompositeDiagramElementsController.java
@@ -2,6 +2,7 @@ package hu.elte.txtuml.export.papyrus.api;
 
 import org.eclipse.papyrus.uml.diagram.common.editparts.RoundedCompartmentEditPart;
 import org.eclipse.papyrus.uml.diagram.composite.edit.parts.ClassCompositeCompartmentEditPart;
+import org.eclipse.uml2.uml.Connector;
 import org.eclipse.uml2.uml.Port;
 import org.eclipse.uml2.uml.Property;
 
@@ -12,12 +13,17 @@ public class CompositeDiagramElementsController {
 	 * @param diagramEditPart
 	 * @param element
 	 */
-	public static void addPropertyToClassCompositeCompartementEditPart(ClassCompositeCompartmentEditPart diagramEditPart, Property element){
+	public static void addPropertyToClassCompositeCompartementEditPart(
+			ClassCompositeCompartmentEditPart diagramEditPart, Property element) {
 		ElementsManagerUtils.addElementToEditPart(diagramEditPart, element);
 	}
 
-	public static void addPortToCompartmentEditPart(RoundedCompartmentEditPart classCompositeNameEditPart,
-			Port p) {
+	public static void addPortToCompartmentEditPart(RoundedCompartmentEditPart classCompositeNameEditPart, Port p) {
 		ElementsManagerUtils.addElementToEditPart(classCompositeNameEditPart, p);
+	}
+
+	public static void addConnectorToCompartmentEditPart(RoundedCompartmentEditPart classCompositeNameEditPart,
+			Connector c) {
+		ElementsManagerUtils.addElementToEditPart(classCompositeNameEditPart, c);
 	}
 }
--- dev/plugins/hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/elementsmanagers/CompositeDiagramElementsManager.java
+++ dev/plugins/hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/elementsmanagers/CompositeDiagramElementsManager.java
@@ -2,7 +2,6 @@ package hu.elte.txtuml.export.papyrus.elementsmanagers;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
@@ -12,7 +11,7 @@ import org.eclipse.papyrus.uml.diagram.composite.custom.utils.CompositeEditPartU
 import org.eclipse.papyrus.uml.diagram.composite.edit.parts.ClassCompositeCompartmentEditPart;
 import org.eclipse.papyrus.uml.diagram.composite.edit.parts.ClassCompositeEditPart;
 import org.eclipse.papyrus.uml.diagram.composite.edit.parts.PropertyPartEditPartCN;
-import org.eclipse.uml2.uml.Classifier;
+import org.eclipse.uml2.uml.Connector;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.Port;
 import org.eclipse.uml2.uml.Property;
@@ -28,54 +27,72 @@ public class CompositeDiagramElementsManager extends AbstractDiagramElementsMana
 
 	@Override
 	public void addElementsToDiagram(List<Element> elements) {
-		//nondeterministic if the top element is generated or not. So drop it, if does not exist.
-		if(this.diagramEditPart.getChildren().size() == 0){
-			Element top = (Element) ((View)this.diagramEditPart.getModel()).getElement();
+		// nondeterministic if the top element is generated or not. So drop it,
+		// if does not exist.
+		if (this.diagramEditPart.getChildren().size() == 0) {
+			Element top = (Element) ((View) this.diagramEditPart.getModel()).getElement();
 			ElementsManagerUtils.addElementToEditPart(this.diagramEditPart, top);
 		}
 		ClassCompositeEditPart composite = (ClassCompositeEditPart) this.diagramEditPart.getChildren().get(0);
-		
-		ClassCompositeCompartmentEditPart compartment = (ClassCompositeCompartmentEditPart) CompositeEditPartUtil.getCompositeCompartmentEditPart(composite);
-		
+
+		ClassCompositeCompartmentEditPart compartment = (ClassCompositeCompartmentEditPart) CompositeEditPartUtil
+				.getCompositeCompartmentEditPart(composite);
+
 		elements.forEach(e -> {
-			if(e instanceof Property){
-				CompositeDiagramElementsController.addPropertyToClassCompositeCompartementEditPart(compartment, (Property)e);				
+			if (e instanceof Property) {
+				CompositeDiagramElementsController.addPropertyToClassCompositeCompartementEditPart(compartment,
+						(Property) e);
 			}
 		});
-		
+
 		@SuppressWarnings("unchecked")
 		List<GraphicalEditPart> children = compartment.getChildren();
-		
-		for(GraphicalEditPart child : children){ 
-			if(child instanceof PropertyPartEditPartCN){
-				addPortToComposite((PropertyPartEditPartCN)child);
-			}
-			
-			if(child instanceof ClassCompositeEditPart){
-				addPortToComposite((ClassCompositeEditPart)child);
-			}
-		};
 
-		
-		addPortToComposite(composite);
-	}
-	
-	@SuppressWarnings("unchecked")
-	private void addPortToComposite(RoundedCompartmentEditPart compartment) {
-		Element elem = (Element) ((View)compartment.getModel()).getElement();
-		List<Port> ports = Collections.emptyList();
-		if(elem instanceof Property){
-			Property prop = (Property) elem;
-			Object clazz =  prop.getType();
-			if(clazz instanceof Classifier){
-				ports = (List<Port>) (Object) ((Classifier)clazz).getAttributes().stream().filter(p-> p instanceof Port).collect(Collectors.toList());
+		for (GraphicalEditPart child : children) {
+			if (child instanceof PropertyPartEditPartCN) {
+				addPortToComposite((PropertyPartEditPartCN) child);
+				addConnector((PropertyPartEditPartCN) child);
 			}
-		}else if(elem instanceof org.eclipse.uml2.uml.Class){
-			ports = (List<Port>) (Object) ((org.eclipse.uml2.uml.Class) elem).getAttributes().stream().filter(p-> p instanceof Port).collect(Collectors.toList());
+
+			if (child instanceof ClassCompositeEditPart) {
+				addPortToComposite((ClassCompositeEditPart) child);
+				addConnector((ClassCompositeEditPart) child);
+			}
 		}
-		
-		for(Port p : ports){
+		;
+
+		addPortToComposite(composite);
+		addConnector(composite);
+	}
+
+	private void addPortToComposite(RoundedCompartmentEditPart compartment) {
+		Element elem = (Element) ((View) compartment.getModel()).getElement();
+		List<Port> ports = Collections.emptyList();
+		if (elem instanceof Property) {
+			Property prop = (Property) elem;
+			Object clazz = prop.getType();
+			if (clazz instanceof org.eclipse.uml2.uml.Class) {
+				ports = ((org.eclipse.uml2.uml.Class) clazz).getOwnedPorts();
+			}
+		} else if (elem instanceof org.eclipse.uml2.uml.Class) {
+			ports = ((org.eclipse.uml2.uml.Class) elem).getOwnedPorts();
+		}
+
+		for (Port p : ports) {
 			CompositeDiagramElementsController.addPortToCompartmentEditPart(compartment, p);
+		}
+	}
+
+	private void addConnector(RoundedCompartmentEditPart composite) {
+		Element elem = (Element) ((View) composite.getModel()).getElement();
+
+		List<Connector> connectors = Collections.emptyList();
+		if (elem instanceof org.eclipse.uml2.uml.Class) {
+			connectors = ((org.eclipse.uml2.uml.Class) elem).getOwnedConnectors();
+		}
+
+		for (Connector p : connectors) {
+			CompositeDiagramElementsController.addConnectorToCompartmentEditPart(composite, p);
 		}
 	}
 }
